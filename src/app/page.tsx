fc18444642dce5ad70c4cb726952309e5e01ab60
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-09-13 18:16:47 +0400

    fix: add use client and wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    
+    <div className={styles.container}>
+
+    </div>
   );
 }
