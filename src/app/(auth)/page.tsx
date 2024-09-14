acf183a0f61425acf82ea8d449cdcee8e35b4725
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-09-14 17:00:01 +0400

    fix

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,10 +1,5 @@
 'use client';
-import ArtistPopup from '../Components/ArtistPopup/ArtistPopup';
-import ArtistPopupBtn from '../Components/ArtistPopup/ArtistPopupBtn/ArtistPopupBtn';
-import ArtistPopupData from '../Components/ArtistPopup/ArtistPopupData/ArtistPopupData';
-import Menu from '../Components/Menu/Menu';
-import PlaylistEditPopup from '../Components/PlaylistEditPopup/PlaylistEditPopup';
-import PopupHeader from '../Components/PopupHeader/PopupHeader';
+
 import styles from './page.module.scss';
 
 
@@ -12,8 +7,7 @@ export default function Home() {
 
     return(
         <main className={styles.main}>
-          {/* <ArtistPopup /> */}
-            <PlaylistEditPopup />
+          
         </main> 
     )
 }
