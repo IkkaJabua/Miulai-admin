7bcc1fe1e89087af4f72982f0cc9e77555c1acbd
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-09-17 19:59:58 +0400

    fix: background color changing after popup up

--- src/app/Components/ArtistPopup/ArtistPopup.tsx
+++ src/app/Components/ArtistPopup/ArtistPopup.tsx
@@ -14,41 +14,44 @@ interface Props {
 const ArtistPopup = (props: Props) => {
     const [isPlaylistEdit, setPlaylistEdit] = useState(false)
 
+
     console.log('Props', props)
     return (
-        <div className={styles.container}>
+        <div className={styles.sss}>
+            <div className={styles.container}>
 
-            <div className={styles.header}>
-                <div onClick={() => setPlaylistEdit(false)} className={styles.img}>
-                    <Image src={'/icon/marcxniv-isari.svg'} alt='image' width={20} height={20} />
+                <div className={styles.header}>
+                    <div onClick={() => setPlaylistEdit(false)} className={styles.img}>
+                        <Image src={'/icon/marcxniv-isari.svg'} alt='image' width={20} height={20} />
+                    </div>
+
+                    <span className={styles.font}>
+                        {props.name}
+                    </span>
+
+                    <div className={styles.img} onClick={props.closeModal}>
+                        <Image src={'/icon/x-gatishva.svg'} alt='close' width={20} height={20} />
+                    </div>
                 </div>
 
-                <span className={styles.font}>
-                    {props.name}
-                </span>
-
-                <div className={styles.img} onClick={props.closeModal}>
-                    <Image src={'/icon/x-gatishva.svg'} alt='close' width={20} height={20} />
+                <div className={styles.wrapper}>
+                    <ArtistPopupData
+                        key1={!isPlaylistEdit ? 'Email' : 'Playlist Name'}
+                        value1={'dolores.chambers@example.com'}
+                        key2={'Registration Date'}
+                        value2={'September 17, 2024 11:22'}
+                        key3={'Playlists Created'}
+                        value3={'4'}
+                        userImage={'/image/userTestImage.png'}
+                        imageStyle={'round'}
+                    />
+                    <ArtistPopupBtn />
                 </div>
+                {!isPlaylistEdit ?
+                    <ArtPopupCards onEdit={() => { setPlaylistEdit(true) }} /> :
+                    <PlaylistTable />
+                }
             </div>
-
-            <div className={styles.wrapper}>
-                <ArtistPopupData
-                    key1={!isPlaylistEdit ? 'Email' : 'Playlist Name'}
-                    value1={'dolores.chambers@example.com'}
-                    key2={'Registration Date'}
-                    value2={'September 17, 2024 11:22'}
-                    key3={'Playlists Created'}
-                    value3={'4'}
-                    userImage={'/image/userTestImage.png'}
-                    imageStyle={'round'}
-                />
-                <ArtistPopupBtn  />
-            </div>
-            {!isPlaylistEdit ?
-                <ArtPopupCards onEdit={() => { setPlaylistEdit(true) }} /> :
-                <PlaylistTable />
-            }
         </div>
     );
 };
