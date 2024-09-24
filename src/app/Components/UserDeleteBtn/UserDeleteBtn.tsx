15b8ddd4b7090952810b6ec34f5dec9e1e5d19af
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-09-24 22:35:01 +0400

    d

--- src/app/Components/UserDeleteBtn/UserDeleteBtn.tsx
+++ src/app/Components/UserDeleteBtn/UserDeleteBtn.tsx
@@ -4,6 +4,44 @@ import Button from '../Button/Button';
 import styles from './UserDeleteBtn.module.scss';
 
 type Props = {
+    onDeleteClick?: () => void;
+    id?: number;
+}
+
+const UserDeleteBtn = (props: Props) => {
+
+    // const [users, setUsers] = useState([])
+    const fetching = () => {
+        axios.get('https://interstellar-1-pdzj.onrender.com/user')
+            // .then((result) => {
+            //     setUsers(result.data)
+
+            // })
+    }
+    useEffect(fetching, [])
+
+
+    const userDelete = async () => {
+        axios.delete(`https://interstellar-1-pdzj.onrender.com/user/${props.id}`)
+            .then(()=> {
+                fetching()
+            })
+        props.onDeleteClick?.()
+
+    }
+
+    return(
+        <Button onClick={userDelete} title={'Delete'} className={styles.button} image={'/icon/trash.svg'} mode='unset' />
+    )
+}
+
+
+export default UserDeleteBtn;import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Button from '../Button/Button';
+import styles from './UserDeleteBtn.module.scss';
+
+type Props = {
     // onDeleteClick?: () => void;
     id?: number;
 }
@@ -13,11 +51,11 @@ const UserDeleteBtn = (props: Props) => {
     // const [users, setUsers] = useState([])
     // const fetching = () => {
     //     axios.get('https://interstellar-1-pdzj.onrender.com/user')
-    //         // .then((result) => {
-    //         //     setUsers(result.data)
-
-    //         // })
+    //         .then((result) => {
+    //             setUsers(result.data)
+    //         })
     // }
+
     // useEffect(fetching, [])
 
   
