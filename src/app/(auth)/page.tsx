5b6747a75b78e0a126529a7c13a837c6c9dd2cc8
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-09-26 20:09:38 +0400

    Update page.tsx

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,17 +1,16 @@
-'use client';
-import styles from './page.module.scss';
-import Header from '../Components/Header/Header';
-import Usertable from '../Components/userTable/usertable';
+"use client";
+import styles from "./page.module.scss";
+import Header from "../Components/Header/Header";
+import Usertable from "../Components/userTable/usertable";
 
 export default function Home() {
-    return (
-        <main className={styles.main}>       
-            <Header />
-            <div className={styles.user}>
-                <h1 className={styles.h1}>User Managment</h1>
-            </div>
-            
-            <Usertable />
-        </main>
-    );
+  return (
+    <main className={styles.main}>
+      <Header />
+      <div className={styles.user}>
+        <h1 className={styles.h1}>User Managment</h1>
+      </div>
+      <Usertable />
+    </main>
+  );
 }
