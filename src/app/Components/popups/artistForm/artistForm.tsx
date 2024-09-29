e09c07e5a161c9c05a5e39e5e5216d0479c98393
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-09-29 16:49:20 +0400

    fix

--- src/app/Components/popups/artistForm/artistForm.tsx
+++ src/app/Components/popups/artistForm/artistForm.tsx
@@ -1,6 +1,4 @@
-// import { Button } from 'antd'
-<<<<<<< HEAD
-<<<<<<< HEAD
+'use client'
 import styles from './artistForm.module.scss'
 import { useForm, SubmitHandler } from "react-hook-form"
 import Button from '../../Button/Button'
@@ -9,244 +7,11 @@ import { useEffect, useState } from 'react'
 import AddAlbum from '../addAlbum/addAlbum'
 import axios from 'axios'
 
-=======
-=======
->>>>>>> parent of d636ebb (build err fxd)
-import styles from "./artistForm.module.scss";
-import { useForm, SubmitHandler } from "react-hook-form";
-import Button from "../../Button/Button";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import AddAlbum from "../addAlbum/addAlbum";
-import axios from "axios";
-<<<<<<< HEAD
->>>>>>> parent of cea225b (Merge branch 'master' of https://github.com/IkkaJabua/Miulai-admin)
-=======
->>>>>>> parent of d636ebb (build err fxd)
+
 
 interface Props {
   onClick?: () => void;
 }
-
-// Define the interface for form values
-interface FormValues {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    artistName: string;
-    biography: string;
-    artistPhoto: FileList;
-=======
-  artistName: string;
-  biography: string;
-  artistPhoto: FileList;
->>>>>>> parent of cea225b (Merge branch 'master' of https://github.com/IkkaJabua/Miulai-admin)
-}
-
-const ArtistForm = (props: Props) => {
-    const [deleted, setDeleted] = useState(false)
-    const [addAlbum, setAddAlbum] = useState(false)
-
-<<<<<<< HEAD
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<any>()
-    if (deleted) {
-        return
-
-    }
-
-    if (addAlbum) {
-        return <AddAlbum onDelete={() => setDeleted(true)} />
-    }
-
-    const onSubmit = (values: any) => {
-        
-
-        const data = new FormData()
-        data.append('firstName', String(values.firstName))
-        data.append('lastName', String(values.lastName))
-        data.append('biography', String(values.biography))
-        data.append('file', values.file[0])
-
-        axios.post("https://interstellar-1-pdzj.onrender.com/author", data, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            })
-                .then(response => {
-                    
-                    console.log('Successfully submitted:', response.data);
-                })
-                .catch(error => {
-                    console.error('Error submitting the form:', error.response?.data || error.message);
-
-                });
-
-    }
-
-=======
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<any>();
-  if (deleted) {
-    return;
-  }
->>>>>>> parent of cea225b (Merge branch 'master' of https://github.com/IkkaJabua/Miulai-admin)
-
-    return (
-
-<<<<<<< HEAD
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
-            <div className={styles.header}>
-                <div>
-                    {/* <Image src={'/icon/back.svg'} width={24} height={24} alt='back' /> */}
-                </div>
-                <div>Add New Artist</div>
-                <div className={styles.cursor} onClick={props.onClick}>
-                    <Image src={'/icon/delete.svg'} width={24} height={24} alt='delete' />
-                </div>
-            </div>
-            <div className={styles.wrapper}>
-                <div className={styles.formBody}>
-                    <div className={styles.inputGap}>
-                        <div>Artist Name</div>
-                        <input className={styles.nameInput}
-                            {...register('firstName')}
-                            type='text' />
-
-                    </div>
-                    <div className={styles.inputGap}>
-                        <div>Last Name </div>
-                        <input className={styles.nameInput}
-                            {...register('lastName')}
-                            type='text' />
-
-                    </div>
-                    <div>
-                        <div>Biography</div>
-                        <div className={styles.inputTwo}>
-                            <input className={styles.biographyInput}
-                                {...register('biography')}
-                                type='text' />
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.formBody}>
-                    <div>
-                        <div>Artist Photo</div>
-                        <div className={styles.photoFile}>
-                            <input className={styles.photoInput}
-                                {...register('file')}
-                                id='file-upload-file' type='file' />
-                            <label htmlFor="file-upload-file">
-                                <Image src={'/icon/Screenshots.svg'} width={90} height={90} alt='screenshot' />
-                            </label>
-                        </div>
-                    </div>
-                    <Button onClick={() => setAddAlbum(true)} title={'New Album'} image='/icon/plus.svg' className={styles.button} />
-                </div>
-            </div>
-            <button>
-                Save
-            </button>
-            {/* <Button title={'Save'} className={styles.buttonTwo} /> */}
-        </form>
-    );
-=======
-  const onSubmit = (values: any) => {
-    const data = new FormData();
-    data.append("firstName", String(values.firstName));
-    data.append("lastName", String(values.lastName));
-    data.append("biography", String(values.biography));
-    data.append("file", values.file[0]);
-
-    axios
-      .post("https://interstellar-1-pdzj.onrender.com/author", data, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((response) => {
-
-        console.log("Successfully submitted:", response.data);
-      })
-      .catch((error) => {
-        console.error(
-          "Error submitting the form:",
-          error.response?.data || error.message
-        );
-      });
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
-      <div className={styles.header}>
-        <div>
-          {/* <Image src={'/icon/back.svg'} width={24} height={24} alt='back' /> */}
-        </div>
-        <div>Add New Artist</div>
-        <div className={styles.cursor} onClick={props.onClick}>
-          <Image src={"/icon/delete.svg"} width={24} height={24} alt="delete" />
-        </div>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.formBody}>
-          <div className={styles.inputGap}>
-            <div>Artist Name</div>
-            <input
-              className={styles.nameInput}
-              {...register("firstName")}
-              type="text"
-            />
-          </div>
-          <div className={styles.inputGap}>
-            <div>Last Name </div>
-            <input
-              className={styles.nameInput}
-              {...register("lastName")}
-              type="text"
-            />
-          </div>
-          <div>
-            <div>Biography</div>
-            <div className={styles.inputTwo}>
-              <input
-                className={styles.biographyInput}
-                {...register("biography")}
-                type="text"
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.formBody}>
-          <div>
-            <div>Artist Photo</div>
-            <div className={styles.photoFile}>
-              <input
-                className={styles.photoInput}
-                {...register("file")}
-                id="file-upload-file"
-                type="file"
-              />
-              <label htmlFor="file-upload-file">
-                <Image
-                  src={"/icon/Screenshots.svg"}
-                  width={90}
-                  height={90}
-                  alt="screenshot"
-                />
-              </label>
-            </div>
-=======
-  artistName: string;
-  biography: string;
-  artistPhoto: FileList;
-}
-
 const ArtistForm = (props: Props) => {
   const [deleted, setDeleted] = useState(false);
   const [addAlbum, setAddAlbum] = useState(false);
@@ -257,7 +22,7 @@ const ArtistForm = (props: Props) => {
     formState: { errors },
   } = useForm<any>();
   if (deleted) {
-    return;
+    return null;
   }
 
   if (addAlbum) {
@@ -347,7 +112,6 @@ const ArtistForm = (props: Props) => {
                 />
               </label>
             </div>
->>>>>>> parent of d636ebb (build err fxd)
           </div>
           <Button
             onClick={() => setAddAlbum(true)}
@@ -357,21 +121,11 @@ const ArtistForm = (props: Props) => {
           />
         </div>
       </div>
-<<<<<<< HEAD
       <button className={styles.buttonTwo}>
         Save
-
       </button>
       {/* <Button title={'Save'} className={styles.buttonTwo} /> */}
     </form>
-  );
->>>>>>> parent of cea225b (Merge branch 'master' of https://github.com/IkkaJabua/Miulai-admin)
-=======
-      <button>Save</button>
-      {/* <Button title={'Save'} className={styles.buttonTwo} /> */}
-    </form>
-  );
->>>>>>> parent of d636ebb (build err fxd)
+  )
 };
-
 export default ArtistForm;
