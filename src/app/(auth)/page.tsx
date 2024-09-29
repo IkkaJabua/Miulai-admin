5d81f38d171b6d2bbb29a76a22d45f2d262f085b
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-09-30 02:25:24 +0400

    c

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import styles from "./page.module.scss";
 import Header from "../Components/Header/Header";
-import Usertable from "../Components/userTable/usertable";
+import Usertable from "../Components/UserTable/usertable";
 
 export default function Home() {
   return (
